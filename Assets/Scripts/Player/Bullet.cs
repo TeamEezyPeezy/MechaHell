eddef42cea1e565af9aaf841e30ea6eede2e372a
Apluri <aleksi.h.hietala@tuni.fi>
2021-06-15 11:56:45 +0300

    Add feature to shoot bullets.

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // public GameObject hitAnimation;
+   void OnCollisionEnter2D(Collision2D collision)
+   {
+       // TODO When animation for bullet explosion / disappearing is done, enable these
+       //GameObject animation = Instantiate(hitAnimation, transform.position, Quaternion.identity);
+       //Destroy(animation, 5f);
+       Destroy(gameObject);
+
+    Debug.Log("Collision");
+   }
+}
