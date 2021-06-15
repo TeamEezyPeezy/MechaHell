f484858c0d430fdf05e948d1311f8e96d99608bb
Apluri <aleksi.h.hietala@tuni.fi>
2021-06-15 11:26:20 +0300

    Update unity scene, Implemented player rotation based on mouse position.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,10 @@ public class PlayerMovement : MonoBehaviour
    
    public float moveSpeed = 5f;
    public Rigidbody2D playerRigidBody;
+   public Camera cam;
    Vector2 movement;
+   Vector2 mousePos;
+   float angleOffset = 0f;
 
 
 
@@ -15,21 +18,33 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         GetMovementInputs();
+        GetMousePosition();
     }
 
     void FixedUpdate()
     {
         MovePlayer();
+        RotatePlayer();
     }
 
     void MovePlayer()
     {
         playerRigidBody.MovePosition(playerRigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+    void RotatePlayer()
+    {
+        Vector2 lookDir = mousePos - playerRigidBody.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - angleOffset;
+        playerRigidBody.rotation = angle;
+    }
 
     void GetMovementInputs()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+    }
+    void GetMousePosition()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 }
