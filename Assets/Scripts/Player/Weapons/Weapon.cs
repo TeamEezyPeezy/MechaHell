8e66fdd6a8102a6545d0fe2eea736de62f806091
Apluri <aleksi.h.hietala@tuni.fi>
2021-06-21 12:09:52 +0300

    Recreate weaponsystem to support weapon switching.
    Test version that supports simple weapon swap by pressing Q. Known bugs: allows weapon swap to same weapon, causing it to refresh ammo. Cannot swap back to other weapons. Code repetition in gunsystem.

---
+++ Assets/Scripts/Player/Weapons/Weapon.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// superclass for weapons to hold weapon spesific data
+public abstract class Weapon
+{
+    public float bulletForce;
+    public int damage;
+    public float timeBetweenShooting;
+    public float spread;
+    public float reloadTime;
+    public float timeBetweenShots;
+    public int magazineSize;
+    public int bulletsPerTap;
+    public bool allowButtonHold;
+
+}
