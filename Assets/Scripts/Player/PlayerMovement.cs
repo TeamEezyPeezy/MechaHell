77ba449e417b57e768494f51b662edc0ec15d42e
Apluri <aleksi.h.hietala@tuni.fi>
2021-06-29 12:23:02 +0300

    Add dash functionality from spacebar

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,41 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class PlayerMovement : MonoBehaviour
 {
    
+   private enum State {
+       Normal,
+       Dashing,
+   }
    public float moveSpeed = 5f;
+   public float dashStartSpeed;
+   public float dashSpeedDropMultiplier;
+   public float dashCooldown;
+   public TextMeshProUGUI dashCooldownTimer;
+   float dashSpeed;
    public Rigidbody2D playerRigidBody;
    public Camera cam;
 
     public Animator animator;
 
+
+    float dashStartTime;
    Vector2 movement;
+   Vector3 dashDirection;
    Vector2 mousePos;
    float angleOffset = -90f;
 
+   State state;
+
+
     bool isWalking = false;
     bool animationChanged = false;
+    bool isDashButtonDown;
+    bool dashOnCooldown;
     void Awake()
     {
-        // animator = GetComponent<Animator>();
+        state = State.Normal;
+        dashOnCooldown = false;
+        dashCooldownTimer.SetText("Dash ready!");
+
     }
     void Update()
     {
         GetMovementInputs();
         GetMousePosition();
         HandleMovementAnimations();
+        HandleCooldowns();
         
-        // temporary reset for testing purposes
-        if(Input.GetButtonDown("Jump")) SceneManager.LoadScene("DoorTestScene");
+      
+    }
+
+    void HandleCooldowns()
+    {
+        if(dashOnCooldown)
+        {
+            dashCooldownTimer.SetText(dashCooldown - ((int)(Time.time - dashStartTime)) + "s");
+            if(Time.time - dashStartTime > dashCooldown)
+            {
+                dashOnCooldown = false;
+                dashCooldownTimer.SetText("Dash ready!");
+            }
+        }
     }
 
     void HandleMovementAnimations()
     {
         if(animationChanged)
         {
-            Debug.Log("animation change");
             animator.SetBool("isMoving", isWalking);
             animationChanged = false;
         }
@@ -49,7 +83,14 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        playerRigidBody.MovePosition(playerRigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+        switch(state){
+            case State.Normal:
+                playerRigidBody.MovePosition(playerRigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+                break;
+            case State.Dashing:
+                playerRigidBody.velocity = movement * dashSpeed;;
+            break;
+        }
         
     }
     void RotatePlayer()
@@ -61,23 +102,51 @@ public class PlayerMovement : MonoBehaviour
 
     void GetMovementInputs()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        if (movement.x != 0 || movement.y != 0)
-        {
-            if(!isWalking){
-                animationChanged = true;
-                isWalking = true;
-            }
-        } else
-        {
-            if(isWalking){
-                animationChanged = true;
-                isWalking = false;
-            }
-        }
+        switch(state){
+            case State.Normal:
+            {
+                movement.x = Input.GetAxisRaw("Horizontal");
+                movement.y = Input.GetAxisRaw("Vertical");
+                if (movement.x != 0 || movement.y != 0)
+                {
+                    if(!isWalking){
+                        animationChanged = true;
+                        isWalking = true;
+                    }
+                } else
+                {
+                    if(isWalking){
+                        animationChanged = true;
+                        isWalking = false;
+                    }
+                }
 
-  
+                if(Input.GetButtonDown("Jump")){
+                    // dash
+                    if(!dashOnCooldown)
+                    {
+                        isDashButtonDown = true;
+                        dashSpeed = 100f;
+                        state = State.Dashing;
+                        dashStartTime = Time.time;
+                        dashOnCooldown = true;
+
+                        // start dash animation here
+                    }
+                    
+                }
+                break;
+            }
+            case State.Dashing:
+                dashSpeed -= dashSpeed * dashSpeedDropMultiplier * Time.deltaTime;
+
+                float dashSpeedMinimum = moveSpeed;
+                if (dashSpeed < dashSpeedMinimum) {
+                    state = State.Normal;
+                    // end dash animation here
+                }
+                break;
+        }
     }
     void GetMousePosition()
     {
