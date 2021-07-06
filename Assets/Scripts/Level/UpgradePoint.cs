6eeb2d614409fcc29ceaac2a77e8e391b0593348
Apluri <aleksi.h.hietala@tuni.fi>
2021-07-06 11:42:14 +0300

    Add upgrade center with simple possibility to open and close it.

---
+++ Assets/Scripts/Level/UpgradePoint.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradePoint : MonoBehaviour
+{
+  
+    public GameObject infoText;
+
+    bool isActive = false;
+    bool playerClose = false;
+
+    void Update()
+    {
+        UpdateInputs();
+    }
+
+    void UpdateInputs()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(!isActive)
+            {
+                isActive = true;
+                OpenPanel();  
+            } else {
+                isActive = false;
+                ClosePanel();
+            }
+        }
+ 
+    }
+
+    void OpenPanel()
+    {
+        infoText.SetActive(false);
+        Debug.Log("panel open");
+    }
+
+    void ClosePanel()
+    {
+        infoText.SetActive(true);
+        Debug.Log("panel close");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        infoText.SetActive(true);
+        playerClose = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        infoText.SetActive(false);
+        playerClose = false;
+    }
+}
