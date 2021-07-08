b4823dfb3f98a4fae504fa08b488adea4884912d
Apluri <aleksi.h.hietala@tuni.fi>
2021-07-08 11:34:13 +0300

    Add upgradecosts to upgrades

--- Assets/Scripts/Level/UpgradePoint.cs
+++ Assets/Scripts/Level/UpgradePoint.cs
@@ -20,14 +20,34 @@ public class UpgradePoint : MonoBehaviour
     public TextMeshProUGUI dashStats;
     public TextMeshProUGUI bazookaStats;
 
+    public TextMeshProUGUI machineGunCostInfo;
+    public TextMeshProUGUI sniperCostInfo;
+    public TextMeshProUGUI bazookaCostInfo;
+    public TextMeshProUGUI dashCostInfo;
+    public TextMeshProUGUI playerCostInfo;
+
+    private GameManager gameManager;
     bool isPanelOpen = false;
     bool playerClose = false;
+
+    int machinegunUpgradeCost = 300;
+    int sniperUpgraceCost = 300;
+    int bazookaUpgradeCost = 300;
+    int dashUpgradeCost = 300;
+    int playerUpgradeCost = 300;
 
     void Awake()
     {
         sniperReference = gunSystem.sniper;
         machinegunReference = gunSystem.machinegun;
+        gameManager = GameManager.Instance;
         UpdateTexts();
+
+        machineGunCostInfo.SetText(machinegunUpgradeCost + "$");
+        sniperCostInfo.SetText(sniperUpgraceCost + "$");
+        bazookaCostInfo.SetText(bazookaUpgradeCost+ "$");
+        dashCostInfo.SetText(dashUpgradeCost + "$");
+        playerCostInfo.SetText(playerUpgradeCost + "$");
     }
     void Update()
     {
@@ -39,8 +59,9 @@ public class UpgradePoint : MonoBehaviour
         machineGunStats.SetText("Machinegun level " + machinegunReference.upgradeLevel);
         sniperStats.SetText("Sniper level " + sniperReference.upgradeLevel);
         playerMovementStats.SetText("Player movementspeed " + playerMovementReference.moveSpeed);
-        bazookaStats.SetText("Bazooka cooldown " + bazookaReference.cooldown);
-        dashStats.SetText("Dash cooldown " + playerMovementReference.dashCooldown);
+        bazookaStats.SetText("Bazooka cooldown " + bazookaReference.cooldown + "s");
+        dashStats.SetText("Dash cooldown " + playerMovementReference.dashCooldown + "s");
+
     }
       void UpdateInputs()
     {
@@ -90,52 +111,90 @@ public class UpgradePoint : MonoBehaviour
 
     public void UpgradeMachineGun()
     {
-        if(machinegunReference.upgradeLevel < 3){
+        if(machinegunReference.upgradeLevel < 3  && hasEnoughMoneyFor(machinegunUpgradeCost)){
+
+            gameManager.Money -= machinegunUpgradeCost;
+            machinegunUpgradeCost += 150;
+
             machinegunReference.magazineSize += 5;
             machinegunReference.timeBetweenShooting *= 0.7f;
             machinegunReference.timeBetweenShots *= 0.7f;
             machinegunReference.spread -= 0.02f;
             machinegunReference.upgradeLevel++;
             gunSystem.UpdateWeaponInfo();
-
             UpdateTexts();
+
+            CostInfoUpdateHandler(machineGunCostInfo, machinegunReference.upgradeLevel == 3, machinegunUpgradeCost);
+
         }
     }
     public void UpgradeSniper()
     {
-        if(sniperReference.upgradeLevel < 3){
+        if(sniperReference.upgradeLevel < 3 && hasEnoughMoneyFor(sniperUpgraceCost)){
+            gameManager.Money -= sniperUpgraceCost;
+            sniperUpgraceCost += 150;
+
             sniperReference.magazineSize += 1;
             sniperReference.timeBetweenShooting *= 0.8f;
             sniperReference.reloadTime *= 0.8f;
             sniperReference.upgradeLevel++;
             gunSystem.UpdateWeaponInfo();
             UpdateTexts();
+
+            CostInfoUpdateHandler(sniperCostInfo, sniperReference.upgradeLevel == 3, sniperUpgraceCost);
         }
     }
     public void UpgradeBazooka()
     {
-        if(bazookaReference.cooldown > 1f){
+        if(bazookaReference.cooldown > 1f && hasEnoughMoneyFor(bazookaUpgradeCost)){
+            gameManager.Money -= bazookaUpgradeCost;
+            bazookaUpgradeCost += 150;
+
             bazookaReference.cooldown -= 1f;
             UpdateTexts();
+            
+            CostInfoUpdateHandler(bazookaCostInfo, bazookaReference.cooldown == 1f, bazookaUpgradeCost);
         }
     }
     public void UpgradeDash()
     {
-        if(playerMovementReference.dashCooldown > 1){
+        if(playerMovementReference.dashCooldown > 1 && hasEnoughMoneyFor(dashUpgradeCost)){
+            gameManager.Money -= dashUpgradeCost;
+            dashUpgradeCost += 150;
+
             playerMovementReference.dashCooldown  -= 1f;
             UpdateTexts();
+
+            CostInfoUpdateHandler(dashCostInfo, playerMovementReference.dashCooldown == 1f, dashUpgradeCost);
         }
        
     }
 
     public void UpgradePlayerStats()
     {
-        if(playerMovementReference.moveSpeed < 10f)
-        {
+        if(playerMovementReference.moveSpeed < 10f && hasEnoughMoneyFor(playerUpgradeCost))
+        {   
+            gameManager.Money -= playerUpgradeCost;
+            playerUpgradeCost += 150;
             playerMovementReference.moveSpeed += 1f;
             UpdateTexts();
+
+            CostInfoUpdateHandler(playerCostInfo, playerMovementReference.moveSpeed == 10f, playerUpgradeCost);
         }
     }
 
+    bool hasEnoughMoneyFor(int amount)
+    {
+        return gameManager.Money > amount;
+    }
+
+    void CostInfoUpdateHandler(TextMeshProUGUI text, bool isMaxed, int cost)
+    {
+        if(isMaxed){
+            text.SetText("MAX");
+        } else {
+            text.SetText(cost + "$");
+        }
+    }
   
 }
--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -108,4 +108,5 @@ public class GameManager : Singleton<GameManager>
     {
         EnemyCount++;
     }
+
 }
