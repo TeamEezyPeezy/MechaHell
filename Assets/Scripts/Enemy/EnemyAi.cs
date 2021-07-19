e3aa6934c3bb4b09c307d06c1b78ee0a876aa9af
Aleksi Asikainen <aleksi.asikainen@tuni.fi>
2021-07-19 22:18:44 +0300

    Enemy rotation / animation / sprint functions

--- Assets/Scripts/Enemy/EnemyAi.cs
+++ Assets/Scripts/Enemy/EnemyAi.cs
@@ -1,35 +1,71 @@
+using System;
 using UnityEngine.AI;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyAi : MonoBehaviour
 {
-    [SerializeField] Transform target;
-
+    public Transform target;
+    public Animator animator;
     private NavMeshAgent agent;
-
-    // Attacking
-    public float timeBetweenAttacks;
-    //private bool alreadyAttacked;
     public GameObject projectile;
-
     public LayerMask whatIsPlayer;
 
-    // States
+    private Timer timer;
+    private Timer sprintTimer;
+
+    // Sprint
+    private float agentNormalSpeed;
+    private float agentNormalAccel;
+    public float minTimeSprint, maxTimeSprint;
+    private float randomTimeToSprint;
+    private bool isSprinting = false;
+    public float sprintDuration = 1f;
+    public float boostPercentage;
+
+    public float timeBetweenAttacks;
+    public float rotateSpeed;
     public float attackRange;
     public bool playerInSightRange, playerInAttackRange;
-    bool alreadyAttacked = false;
+
+    public float mySpeed = 10f;
+    public float myAccel = 6f;
+
+    private float myVelocity;
+    private bool alreadyAttacked = false;
+
+    private void Awake()
+    {
+        timer = gameObject.AddComponent<Timer>();
+        sprintTimer = gameObject.AddComponent<Timer>();
+    }
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
-
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        agentNormalSpeed = agent.speed;
+        agentNormalAccel = agent.acceleration;
+        StartSprintRandomTime();
+    }
+
+    void StartSprintRandomTime()
+    {
+        randomTimeToSprint = Random.Range(minTimeSprint, maxTimeSprint);
+        timer.Run(randomTimeToSprint);
     }
 
     void Update()
     {
+        agent.speed = mySpeed;
+        agent.acceleration = myAccel;
+
+        myVelocity = agent.velocity.magnitude;
+
+        animator.SetFloat("Velocity", myVelocity);
+
         playerInAttackRange = CheckAttackRange(transform, target);
 
         if (!playerInAttackRange) ChasePlayer();
@@ -44,40 +80,52 @@ public class EnemyAi : MonoBehaviour
         return false;
     }
 
+    private void Sprinting()
+    {
+        if (timer.IsComplete && !isSprinting)
+        {
+            isSprinting = true;
+            sprintTimer.Run(sprintDuration);
+        }
+
+        if (isSprinting && !sprintTimer.IsComplete)
+        {
+            mySpeed = agentNormalSpeed * (1 + (boostPercentage / 100f));
+            myAccel = agentNormalAccel * (1 + (boostPercentage / 100f));
+        }
+        else
+        {
+            sprintTimer.Stop();
+            agent.speed = agentNormalSpeed;
+            StartSprintRandomTime();
+            isSprinting = false;
+        }
+    }
+
     private void ChasePlayer()
     {
-        agent.SetDestination(target.position);
+        Sprinting();
+
+        if (target != null)
+        {
+            RotateTowards(target.position);
+            agent.SetDestination(target.position);
+        }
     }
 
     private void AttackPlayer()
     {
         agent.SetDestination(transform.position);
-
-        // ROTATE
         RotateTowards(target.position);
 
-        //print(transform.name + "Attacks player");
-
-        /*
         if (!alreadyAttacked)
         {
-            // Attack code here.
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * 15f, ForceMode.Impulse);
-            Physics.IgnoreCollision(rb.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
-            Destroy(rb.gameObject, 5f);
-            //
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
-        } */
+            animator.SetTrigger("Attack");
 
-        if(!alreadyAttacked)
-        {
             alreadyAttacked = true;
             Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, attackRange);
             foreach (Collider2D nearby in colliders)
             {
-           
                 Player player = nearby.gameObject.GetComponent<Player>();
 
                 if (player != null)
@@ -86,9 +134,8 @@ public class EnemyAi : MonoBehaviour
                     Debug.Log("Player took damage");
                 }
             }
-            Invoke("ResetAttack", timeBetweenAttacks);
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
-        
     }
 
     private void ResetAttack()
@@ -102,9 +149,11 @@ public class EnemyAi : MonoBehaviour
         Vector2 direction = target - (Vector2)transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
-    }
 
+        Quaternion currentRotation = transform.rotation;
+        Quaternion nextRotation = Quaternion.Euler(Vector3.forward * (angle + offset));
+        transform.rotation = Quaternion.Lerp(currentRotation, nextRotation, Time.time * (rotateSpeed / 100));
+    }
 
     private void OnDrawGizmosSelected()
     {
